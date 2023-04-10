7ee480384f50e993cfc85e3c51fd90f275352da9
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-04-10 14:34:40 -0300

    alo

--- ex-javascript/ex07.js
+++ ex-javascript/ex07.js
@@ -6,4 +6,33 @@
 // “Delta é negativo”.
 
 
+function bhaskara(ax2, bx, c) {
+    const delta = Math.pow(bx, 2) - 4 * ax2 * c;
+    if (delta < 0) {
+        return "Delta é negativo";
+    }else {
+        const x1 = (-bx + Math.sqrt(delta)) / (2* ax2);
+        const x2 = (-bx - Math.sqrt(delta)) / (2 * ax2);
+        return [x1, x2];
+    }
+}
+const resultado = bhaskara (5, 8, 12);
+console.log(resultado);
 
+// reposta do professor
+
+function bhaskara (ax2, bx, c) {
+    let resultados = []
+    let delta = (bx ** 2) - (4 * ax2 * c)
+    if(delta < 0){
+        return "Delta é negativo"
+    } 
+    let x1 = (-bx + Math.sqrt(delta))/(2*ax2)
+    let x2 = (-bx - Math.sqrt(delta))/(2*ax2)
+    resultados.push(x1)
+    resultados.push(x2)
+    return resultados   
+}
+
+console.log(bhaskara(1, 3, 2))
+console.log(bhaskara(3, 1, 2))
