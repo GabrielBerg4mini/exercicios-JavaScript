8a1025d125a3b29385d078c9b487d4239e706bc2
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-05-01 11:32:50 -0300

    mais um pouco de estudo

--- praticando-js/ex02/index.js
+++ praticando-js/ex02/index.js
@@ -1,5 +1,28 @@
-console.log('Estudar é muito bom');
-console.log('Estudar é muito bom');
-console.log('Estudar é muito bom');
 
-function 
+// const sum = function(n1, n2) {
+//     let total = n1 + n2;
+//     return total
+// };
+
+// let n1 = 34;
+// let n2 = 25;
+
+// console.log(`n1 é ${n1}`);
+// console.log(`n2 é ${n2}`);
+// console.log(`A soma é  ${sum(n1, n2)}`);
+
+// function fazerSuco(fruta1, fruta2) {
+//     return fruta1 + fruta2;
+// };
+
+// const copo = fazerSuco("banana", "maça");
+// console.log(copo);
+
+let subject = 'create video'
+function createThink(subject) {
+    subject = 'study'
+    return subject
+}
+
+console.log(subject);
+console.log(createThink(subject));
