3ad779799122d5dcb8b25cbe8ed7f9e24b008d5a
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-05-03 08:38:48 -0300

    tabalhando o switch

--- praticando-js/ex03.js
+++ praticando-js/ex03.js
@@ -1,14 +1,33 @@
 //ajustando meu conhecimento em condicionais
+// if...else
+// let temperature = 30.5;
+// let highTemperature = temperature >= 37.5;
+// let mediumTemperature = temperature <= 38.5 && temperature >= 36.5;
 
-let temperature = 30.5;
-let highTemperature = temperature >= 37.5;
-let mediumTemperature = temperature <= 38.5 && temperature >= 36.5;
+// if (highTemperature) {
+//     console.log('essa pessoa está com febre alta');
+// }else if(mediumTemperature) {
+//     console.log('febre moderada');
+// }
+//  else {
+//     console.log('essa pessoa está com temperatura normal');
+// };
 
-if (highTemperature) {
-    console.log('essa pessoa está com febre alta');
-}else if(mediumTemperature) {
-    console.log('febre moderada');
+//switch
+
+let expression = ''
+
+switch(expression) {
+    case 'a':
+        // código aqui
+        console.log('a')
+        break
+    case 'b': 
+        //código para expression b  
+        console.log('b')  
+        break
+    default:
+        //vai parar a condicional
+        console.log('default')
+        break
 }
- else {
-    console.log('essa pessoa está com temperatura normal');
-};
