861992e6ed03ea44b4c4834ff0fade739695bd41
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-05-03 08:26:51 -0300

    mais um ex

---
+++ praticando-js/ex03.js
@@ -0,0 +1,9 @@
+//ajustando meu conhecimento em condicionais
+
+let temperature = 39.5;
+
+if (temperature >= 37) {
+    console.log('essa pessoa está com febre')
+} else {
+    console.log('essa pessoa está com temperatura normal')
+}
