29ef962ab5a609f89df5c111efaedf18d3037352
Gabriel Bergamini <gabrielbergaminioficial@gmail.com>
2023-05-22 18:06:50 -0300

    mais uns excs

---
+++ praticando-js/dom/aula01/script.js
@@ -0,0 +1,19 @@
+const element = document.querySelector('body');
+element.style.backgroundColor = 'red'
+
+
+
+// const header = document.querySelector('header');
+// header.setAttribute('id','header');
+
+// const headerID = document.querySelector('#header')
+// console.log(headerID.getAttribute('class'))
+
+// header.removeAttribute('id')
+// header.setAttribute('class', 'bgf')
+
+// const element = document.querySelector('input');
+
+// console.log(element.value)
+
+// element.value = "outro valor"
